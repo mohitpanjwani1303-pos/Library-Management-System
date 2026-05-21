52bf02f3a18d48bcdadc4ebd4782c94235f457ba
mohitpanjwani1303-pos <mohitpanjwani1303@gmail.com>
2026-05-21 17:16:53 +0530

    Add Member class definition in Member.h

---
+++ Member.h
@@ -0,0 +1,15 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#include <string>
+using namespace std;
+
+class Member {
+public:
+    int memberID;
+    string name;
+
+    Member(int id, string n);
+};
+
+#endif
